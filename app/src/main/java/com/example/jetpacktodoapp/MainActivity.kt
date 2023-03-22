1de142d8863e16a0614a192aac9a01db768226d0
sanao1006 <naosak1006@gmail.com>
2023-03-22 22:01:56 +0900

    put dialog to create a task

--- app/src/main/java/com/example/jetpacktodoapp/MainActivity.kt
+++ app/src/main/java/com/example/jetpacktodoapp/MainActivity.kt
@@ -8,8 +8,12 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetpacktodoapp.components.EditDialog
 import com.example.jetpacktodoapp.ui.theme.JetpackTodoAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,8 +36,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainContent() {
+    val isShowDialog = remember { mutableStateOf(false) }
+    if (isShowDialog.value) {
+        EditDialog(isShowDialog)
+    }
     Scaffold(floatingActionButton = {
-        FloatingActionButton(onClick = { /*TODO*/ }) {
+        FloatingActionButton(onClick = { isShowDialog.value = true }) {
             Icon(imageVector = Icons.Default.Add, contentDescription = "新規作成")
         }
     }) {
