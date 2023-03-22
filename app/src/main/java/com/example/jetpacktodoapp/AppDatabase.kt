d464630b7205e7b4e3b6a0bf47fda16a457ff9a6
sanao1006 <naosak1006@gmail.com>
2023-03-22 20:55:57 +0900

    Adding Dependencies  - Hilt

--- app/src/main/java/com/example/jetpacktodoapp/AppDatabase.kt
+++ app/src/main/java/com/example/jetpacktodoapp/AppDatabase.kt
@@ -3,7 +3,7 @@ package com.example.jetpacktodoapp
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [Task::class], version = 1)
+@Database(entities = [Task::class], version = 1, exportSchema = false)
 abstract class AppDatabase: RoomDatabase() {
     abstract fun taskDAO(): TaskDAO
 }
